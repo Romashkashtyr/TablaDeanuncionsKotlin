3ddbcd6bdf15b15b05c92b932cb4e19d18092651
Romashka <romainshtyr@yandex.ru>
2024-03-07 22:08:29 +0300

    Adding EditActivity logic & json file

--- app/src/main/java/com/example/tabladeanuncionskotlin/act/EditAdsAct.kt
+++ app/src/main/java/com/example/tabladeanuncionskotlin/act/EditAdsAct.kt
@@ -2,8 +2,10 @@ package com.example.tabladeanuncionskotlin.act
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.example.tabladeanuncionskotlin.R
 import com.example.tabladeanuncionskotlin.databinding.ActivityEditAdsBinding
+import com.example.tabladeanuncionskotlin.utils.CityHelper
 
 class EditAdsAct : AppCompatActivity() {
     private lateinit var binding: ActivityEditAdsBinding
@@ -11,5 +13,10 @@ class EditAdsAct : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityEditAdsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item,
+            CityHelper.getAllCountries(this))
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spCountry.adapter = adapter
     }
 }
